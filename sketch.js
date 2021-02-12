adb3651f90b63a9d7f4f999be577028df8c31d08
RAKSHIT2006 <72135785+RAKSHIT2006@users.noreply.github.com>
2021-02-12 20:01:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var  database;
+var form,player,game;
+var gameState =0;
+var playerCount;
+
+
+
+function setup(){
+  database = firebase.database();
+  
+  createCanvas(500,500);
+game = new Game();
+game.getState();
+game.start();
+ 
+}
+
+ /* var hypnoticBallPosition = database.ref('ball/position');
+  hypnoticBallPosition.on("value", readPosition, showError);
+}*/
+
+function draw(){
+  
+   
+  
+}
+
+/*function writePosition(x,y){
+  database.ref('ball/position').set({
+    'x': position.x + x ,
+    'y': position.y + y
+  })
+}
+
+function readPosition(data){
+  position = data.val();
+  console.log(position.x);
+  hypnoticBall.x = position.x;
+  hypnoticBall.y = position.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}*/
